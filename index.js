a8e286c469c90b1648a0e6ab24ebcbcfcc77f24f
konateb <111666644+konateb@users.noreply.github.com>
2024-04-24 11:46:53 -0400

    clean up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //index.js
 import productEmitter from "./sendEventNotification.js";
 import * as readline from "readline";
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -17,6 +16,7 @@ rl.question("Nom du produit: ", (name) => {
     console.log("L'objet produit :", newProduct);
     rl.close();
     // emettre evenement d'ajout du produit
-    productEmitter.emit("newProductAdded", newProduct);
+     productEmitter.emit("newProductAdded", newProduct);
   });
 });
+
