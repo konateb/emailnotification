3884e28e7efb5675f177f9e80985c8963e04eacb
konateb <111666644+konateb@users.noreply.github.com>
2024-04-25 20:12:45 -0400

    add .env-local

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 //index.js
-import productEmitter from "./sendEventNotification.js";
 import * as readline from "readline";
+import productEmitter from "./sendEventNotification.js"
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-//create a produit to send.
+//create objet produit to send.
 const newProduct = {};
-// entrer les attributs du produit
+// taper les info du nouveau produit
 rl.question("Nom du produit: ", (name) => {
   newProduct.name = name;
 
   rl.question("Prix: ", (price) => {
     newProduct.price = parseFloat(price);
-    console.log("L'objet produit :", newProduct);
+    console.log("L'objet produit chargé en mémoire :", newProduct);
     rl.close();
     // emettre evenement d'ajout du produit
-     productEmitter.emit("newProductAdded", newProduct);
+    productEmitter.emit("newProductAdded", newProduct);
   });
 });
-
